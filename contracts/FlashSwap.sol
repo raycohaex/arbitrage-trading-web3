0317ce02991be75596db916c99e8b3bdcc348735
Rayco Haex <raycoh1299@gmail.com>
2023-02-07 17:43:45 +0100

    cleaned up project

---
+++ contracts/FlashSwap.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.6.6;
+
+import "hardhat/console.sol";
+
+contract FlashSwap {
+    function flashSwap() public {
+        console.log("Hello, world!");
+    }
+}
