f774ecc86167b762bc078113b9dbd1873ad3b88e
Rayco Haex <raycoh1299@gmail.com>
2023-02-07 17:00:46 +0100

    added project config options

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,42 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const defaultConfig = {
+  optimizer: {
+    enabled: true,
+    runs: 200,
+  }
+}
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.5",
+        settings: defaultConfig,
+      },
+      {
+        version: "0.6.6",
+        settings: defaultConfig,
+      },
+      {
+        version: "0.8.8",
+        settings: defaultConfig,
+      },
+    ]
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        // https://docs.bnbchain.org/docs/rpc/
+        url: "https://bsc-dataseed.binance.org"
+      }
+    },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  }
 };
